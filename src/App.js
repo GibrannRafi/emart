0fa34ad3a96947d4814aff96fd88358449e32cb3
GibrannRafi <gibranrafihamdani@gmail.com>
2022-03-18 01:02:30 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import {Switch, Route} from 'react-router-dom'
+import Products from './components/Products';
+import Product from './components/Product';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route  path='/products' component={Products}/>
+      <Route path='/products/:id' component={Product}/>
+    </Switch>
+    </>
+  );
+}
+
+export default App;
